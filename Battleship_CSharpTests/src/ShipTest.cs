4ece732c38970aec03f34b35767fe9aa4cdd4185
alexwong817 <alexwong817@hotmail.com>
2016-10-05 18:32:20 +1100

    UnitTest

---
+++ Battleship_CSharpTests/src/ShipTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+	[TestFixture ()]
+	public class ShipTest
+	{
+		[Test()]
+		public void newShipAdded()
+		{
+			Ship s = new Ship (ShipName.Tug);
+			Assert.AreEqual ("Tug", s.Name);
+		}
+
+		[Test()]
+		public void sizeOfShip()
+		{
+			Ship s = new Ship (ShipName.Tug);
+			Assert.AreEqual(1, s.Size);
+		}
+
+		[Test()]
+		public void Score()
+		{
+			Ship s = new Ship (ShipName.Tug);
+			BattleShipsGame bs = new BattleShipsGame ();
+			Player p = new Player (bs);
+			Tile t = new Tile (0,0,s);
+			s.AddTile (t);
+			s.Hit ();
+			int expect = 0;
+			int actual = p.Score;
+
+			Assert.AreEqual(expect, actual);
+		}
+	}
+}
+
